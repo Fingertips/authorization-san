7c660c631516c1d5bcae4f020d4084ae6170d870
Manfred Stienstra <manfred@fngtps.com>
2009-06-15 13:53:04 +0200

    Fix a mistake in the scope example.

--- examples/administrations_controller.rb
+++ examples/administrations_controller.rb
@@ -3,7 +3,7 @@ class PagesController < ApplicationController
   # The following rule only allows @authenticated if @authenticated.organization.id == params[:organization_id].
   # Roughly translated this means that the authenticated user can only access resources belonging to its own
   # organization.
-  allow_access :authenticated, :scope => :article
+  allow_access :authenticated, :scope => :organization
 
   def index
     @administrations = @authenticated.organization.administrations
