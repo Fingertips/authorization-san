8c1b04e63c2bde1e92c9c6404a71c88f93def00b
Hrvoje Šimić <shime.ferovac@gmail.com>
2012-08-19 00:05:58 +0200

    make tests passing on 1.9.3

--- test/test_helper/rails2/test_helper.rb
+++ test/test_helper/rails2/test_helper.rb
@@ -5,22 +5,32 @@ module AuthorizationSanTest
     def self.load_dependencies
       require 'rubygems'
       gem 'rails', '< 3.0'
-      
+
       require 'test/unit'
-      
+
       require 'active_support'
       require 'active_support/test_case'
       require 'active_record'
       require 'active_record/test_case'
       require 'active_record/base' # this is needed because of dependency hell
       require 'action_controller'
-      
+
       $:.unshift File.expand_path('../../lib', __FILE__)
       require File.join(PLUGIN_ROOT, 'rails', 'init')
-      
+
       puts "{!} Running on Rails 2"
     end
   end
 end
 
+
+module ActiveSupport
+  module Dependencies
+    def load_without_new_constant_marking(*args,&blk)
+      super(*args,&blk)
+    rescue LoadError
+      nil
+    end
+  end
+end
 AuthorizationSanTest::Initializer.start
