9894f0541d9ae99f9a07da3577d4c52c77ffc6d2
Manfred Stienstra <manfred@fngtps.com>
2013-05-19 13:11:18 +0200

    Make gem install work on Travis.

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-gem install rails --version "~> 2.3.0"
-gem install rails --version "~> 3.0.0"
-gem install rails --version "~> 3.1.0"
-gem install rails --version "~> 3.2.0"
+yes |gem install rails --version "~> 2.3.0"
+yes |gem install rails --version "~> 3.0.0"
+yes |gem install rails --version "~> 3.1.0"
+yes | gem install rails --version "~> 3.2.0"
