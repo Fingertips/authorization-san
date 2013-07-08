8dd180c3145a811a44eaa518aa9a34c8d42c7928
Manfred Stienstra <manfred@fngtps.com>
2013-07-08 15:57:18 +0200

    Whitespace in install script.

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-yes |gem install rails --version "~> 2.3.0"
-yes |gem install rails --version "~> 3.0.0"
-yes |gem install rails --version "~> 3.1.0"
+yes | gem install rails --version "~> 2.3.0"
+yes | gem install rails --version "~> 3.0.0"
+yes | gem install rails --version "~> 3.1.0"
 yes | gem install rails --version "~> 3.2.0"
