937478b078d397e31127d8bfe2517603b851c5be
Manfred Stienstra <manfred@fngtps.com>
2014-03-21 15:38:21 +0100

    Attempt to remove Rails bin on Travis.

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ if [ $ruby_version -eq 1 ]; then
   yes | gem install rails --version "~> 2.3.0"
   yes | gem install rails --version "~> 3.0.0"
   yes | gem install rails --version "~> 3.1.0"
+  rm -f `which rails`
   yes | gem install rails --version "~> 3.2.0"
 else
   yes | gem install rails --version "~> 3.2.0"
