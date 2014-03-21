cae51de41ede03c49383aca3a9d8f1eb3ed71df2
Manfred Stienstra <manfred@fngtps.com>
2014-03-21 15:19:37 +0100

    Install Rails version depending on Ruby version.

--- install.sh
+++ install.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
-yes | gem install rails --version "~> 2.3.0"
-yes | gem install rails --version "~> 3.0.0"
-yes | gem install rails --version "~> 3.1.0"
-yes | gem install rails --version "~> 3.2.0"
-yes | gem install rails --version "~> 4.0.0"
+ruby_version=`ruby -e "print(RUBY_VERSION < '2.0.0' ? '1' : '0' )"`
+
+if [ $ruby_version -eq 1 ]; then
+  yes | gem install rails --version "~> 2.3.0"
+  yes | gem install rails --version "~> 3.0.0"
+  yes | gem install rails --version "~> 3.1.0"
+else
+  yes | gem install rails --version "~> 3.2.0"
+  yes | gem install rails --version "~> 4.0.0"
+fi
